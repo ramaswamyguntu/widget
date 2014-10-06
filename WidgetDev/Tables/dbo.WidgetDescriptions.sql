d90c3d643882c7ca484b77e294afeae4bb71aa7f
ramamotifworks <ramaswamy@motifworks.com>
2014-10-06 17:09:57 +0530

    Dropped CK

--- WidgetDev/Tables/dbo.WidgetDescriptions.sql
+++ WidgetDev/Tables/dbo.WidgetDescriptions.sql
@@ -4,7 +4,6 @@ CREATE TABLE [dbo].[WidgetDescriptions]
 [Description] [varchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [WidgetName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
-ALTER TABLE [dbo].[WidgetDescriptions] ADD
-CONSTRAINT [CK_WidgetName_NOTNULL] CHECK (([WidgetName] IS NOT NULL))
+
 
 GO
